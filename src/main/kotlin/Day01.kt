58a4a9276ff197aa0862a905e3757d0407049653
mkulak <kleshney@gmail.com>
2023-12-01 18:29:22 +0100

    remove "zero" handling

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -9,10 +9,8 @@ private fun part1(input: List<String>): Int = input.sumOf { line ->
 }
 
 private fun part2(input: List<String>): Int {
-    val words = listOf(
-        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
-    )
-    val map = words.mapIndexed { i, word -> word to i }.toMap() + List(10) { "$it" to it}.toMap()
+    val words = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    val map = words.mapIndexed { i, word -> word to (i + 1) }.toMap() + List(10) { "$it" to it}.toMap()
     return input.sumOf { line ->
         val first = map.keys.minBy { line.indexOf(it).takeIf { it != -1 } ?: Int.MAX_VALUE }
         val last = map.keys.maxBy { line.lastIndexOf(it) }
