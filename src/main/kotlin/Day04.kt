8d417f624559e8a4eb4477ad64bebae0e61a0380
mkulak <kleshney@gmail.com>
2023-12-04 12:25:41 +0100

    add solution for day 4 part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -6,12 +6,26 @@ fun main() {
     println(part2(input))
 }
 
-private fun part1(input: List<String>): Long = input.sumOf { line ->
-    val (win, has) = line.replace("Card(\\s+)(\\d+):".toRegex(), "").split("|").map { part ->
-        part.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
-    }
-    2.0.pow(has.count { it in win } - 1).toLong()
+private fun part1(input: List<String>): Int = input.sumOf { line ->
+    val (win, has) = parseCards(line)
+    2.0.pow(has.count { it in win } - 1).toInt()
 }
 
-private fun part2(input: List<String>): Int = 0
+private fun part2(input: List<String>): Int {
+    val points = input.map { line ->
+        val (win, has) = parseCards(line)
+        has.count { it in win }
+    }
+    val cards = IntArray(points.size) { 1 }
+    for (i in cards.indices) {
+        for (j in 1..points[i]) {
+            cards[i + j] = cards[i + j] + cards[i]
+        }
+    }
+    return cards.sum()
+}
+
+private fun parseCards(line: String) = line.replace("Card(\\s+)(\\d+):".toRegex(), "").split("|").map { part ->
+    part.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
+}
 
