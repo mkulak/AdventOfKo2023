aa7263ef26e6a668159246676e4d4ec7180243f6
mkulak <kleshney@gmail.com>
2023-12-04 12:13:01 +0100

    beautify solution for day 4 part 1

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,4 +1,3 @@
-import kotlin.math.max
 import kotlin.math.pow
 
 fun main() {
@@ -8,10 +7,10 @@ fun main() {
 }
 
 private fun part1(input: List<String>): Long = input.sumOf { line ->
-    val (win, has) = line.replace("Card(\\s+)(\\d+):".toRegex(), "").split("|")
-    val winSet = win.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
-    val winCount = has.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.count { it in winSet }
-    2.0.pow(winCount - 1).toLong()
+    val (win, has) = line.replace("Card(\\s+)(\\d+):".toRegex(), "").split("|").map { part ->
+        part.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }.toSet()
+    }
+    2.0.pow(has.count { it in win } - 1).toLong()
 }
 
 private fun part2(input: List<String>): Int = 0
