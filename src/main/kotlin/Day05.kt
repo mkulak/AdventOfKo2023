08326fc511ca779b602c68334abb8b3ec739f2dd
mkulak <kleshney@gmail.com>
2023-12-06 13:14:01 +0100

    beautify solution for day 5

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -9,59 +9,46 @@ fun main() {
 
 private fun part1(input: String): Long {
     val sections = input.split("\n\n")
-    val seeds = sections[0].removePrefix("seeds: ").split(" ").map { it.toLong() }
-    val layers = parseMappingLayers(sections)
-    return seeds.map { findLocation(it, layers) }.min()
+    val seeds = sections[0].removePrefix("seeds: ").split(" ").map { Interval(it.toLong(), it.toLong() + 1, 0) }
+    return solve(seeds, parseLayers(sections))
 }
-
-private fun parseMappingLayers(sections: List<String>): List<List<Mapping>> = sections.drop(1).map { section ->
-    section.lines().drop(1).map { line ->
-        val (dst, src, size) = line.split(" ").map { it.toLong() }
-        Mapping(src, dst, size)
-    }
-}
-
-private fun findLocation(seed: Long, layers: List<List<Mapping>>): Long = layers.fold(seed) { acc, layer ->
-    acc + (layer.find { acc >= it.src && acc < (it.src + it.size) }?.let { it.dst - it.src } ?: 0)
-}
-
-data class Mapping(val src: Long, val dst: Long, val size: Long)
-
 
 private fun part2(input: String): Long {
     val sections = input.split("\n\n")
     val seeds = sections[0].removePrefix("seeds: ").split(" ").map { it.toLong() }.chunked(2) { (start, size) ->
         Interval(start, start + size, 0)
     }
-    val initial = IntervalTree()
-    seeds.forEach(initial::add)
-    val layers = parseLayers(sections)
-    return layers.fold(initial) { cur, layer ->
-        val res = IntervalTree()
-        cur.data.forEach { interval -> splitRange(interval, layer, res) }
-        res
-    }.data.first.start
+    return solve(seeds, parseLayers(sections))
 }
 
-fun splitRange(interval: Interval, layer: IntervalTree, out: IntervalTree) {
+private fun solve(seeds: List<Interval>, layers: List<IntervalTree>) =
+    layers.fold(seeds) { cur, layer ->
+        cur.flatMap { interval ->
+            splitRange(interval, layer)
+        }
+    }.minOf { it.start }
+
+fun splitRange(interval: Interval, layer: IntervalTree): List<Interval> {
+    val res = ArrayList<Interval>()
     var cur = interval.copy(offset = 0)
     while (cur.start < cur.end) {
         val overlap = layer.findLeftmostOverlap(cur)
         cur = if (overlap == null) {
-            out.add(cur)
-            Interval(interval.end,  interval.end, 0)
+            res += cur
+            cur.copy(start = interval.end)
         } else if (overlap.start <= cur.start) {
             val end = min(overlap.end, cur.end)
-            out.add(Interval(cur.start + overlap.offset, end + overlap.offset, overlap.offset))
-            Interval(end, interval.end, 0)
+            res += Interval(cur.start + overlap.offset, end + overlap.offset, overlap.offset)
+            cur.copy(start = end)
         } else {
-            out.add(Interval(cur.start, overlap.start, 0))
-            Interval(overlap.start, cur.end, 0)
+            res += cur.copy(end = overlap.start)
+            cur.copy(start = overlap.start)
         }
     }
+    return res
 }
 
-private fun parseLayers(sections: List<String>): List<IntervalTree> = sections.drop(1).map { section ->
+private fun parseLayers(sections: List<String>) = sections.drop(1).map { section ->
     section.lines().drop(1).fold(IntervalTree()) { tree, line ->
         val (dst, src, size) = line.split(" ").map { it.toLong() }
         tree.add(Interval(src, src + size, dst - src))
@@ -69,26 +56,20 @@ private fun parseLayers(sections: List<String>): List<IntervalTree> = sections.d
     }
 }
 
-class IntervalTree {
-    val data = TreeSet<Interval>()
-
-    fun add(interval: Interval) {
-        data += interval
+fun IntervalTree.findLeftmostOverlap(interval: Interval): Interval? {
+    val left = floor(interval)
+    if (left != null && interval.start < left.end) {
+        return left
     }
-
-    fun findLeftmostOverlap(interval: Interval): Interval? {
-        val left = data.floor(interval)
-        if (left != null && interval.start < left.end) {
-            return left
-        }
-        val right = data.ceiling(interval)
-        if (right != null && interval.end > right.start) {
-            return right
-        }
-        return null
+    val right = ceiling(interval)
+    if (right != null && interval.end > right.start) {
+        return right
     }
+    return null
 }
 
 data class Interval(val start: Long, val end: Long, val offset: Long) : Comparable<Interval> {
     override fun compareTo(other: Interval): Int = start.compareTo(other.start)
 }
+
+typealias IntervalTree = TreeSet<Interval>
