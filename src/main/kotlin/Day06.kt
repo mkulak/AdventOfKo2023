55316558db2720a47b81353fe413fe63d5265e9c
mkulak <kleshney@gmail.com>
2023-12-06 18:24:34 +0100

    beautify solution for day 6

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,19 +1,14 @@
 fun main() {
-    val times = listOf(41, 77, 70, 96)
-    val distance = listOf(249, 1362, 1127, 1011)
+    val times = listOf<Long>(41, 77, 70, 96)
+    val distance = listOf<Long>(249, 1362, 1127, 1011)
     println(part1(times.zip(distance)))
     println(part2(41777096L, 249136211271011L))
 }
 
-private fun part1(input: List<Pair<Int, Int>>): Int = input.map { (maxTime, distance) ->
-    (1..<maxTime).count { (maxTime - it) * it > distance }
-}.reduce { a, b -> a * b }
+private fun part1(input: List<Pair<Long, Long>>): Int =
+    input.map { (time, distance) -> solve(time, distance) }.reduce { a, b -> a * b }
 
-private fun part2(time: Long, distance: Long): Long {
-    var count = 0L
-    (1..<time).forEach {
-        if ((time - it) * it > distance) count++
-    }
-    return count
-}
+private fun part2(time: Long, distance: Long): Int = solve(time, distance)
+
+private fun solve(time: Long, distance: Long) = (1..<time).count { (time - it) * it > distance }
 
