94f23fb19357d8e6af88c75161a4dc02d4627987
mkulak <kleshney@gmail.com>
2023-12-07 20:51:37 +0100

    beautify solution for day 7 part 1

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,24 +1,41 @@
+import java.util.Comparator
+
 fun main() {
     val input = readInput("Day07")
     println(part1(input))
     println(part2(input))
 }
 
-private fun part1(input: List<String>): Int = input.map { line ->
-    val (cards, bid) = line.split(" ")
-    cards to bid.trim().toInt()
-}.sortedWith(comparator).mapIndexed { i, hand -> (i + 1) * hand.second }.sum()
+private fun part1(input: List<String>): Int {
+    fun calculateStrength(hand: String): Int =
+        hand.fold(0) { acc, ch -> acc * 14 + "23456789TJQKA".indexOf(ch) }
 
-fun calculateType(hand: Pair<String, Int>): Int =
-    types.indexOf(hand.first.groupBy { it }.values.map { it.size }.sorted())
+    val comparator = compareBy(::calculateType).thenComparing(::calculateStrength)
+    return solve(input, comparator)
+}
 
-fun calculateStrength(hand: Pair<String, Int>): Int =
-    hand.first.fold(0) { acc, ch -> acc * 14 + "23456789TJQKA".indexOf(ch) }
+private fun part2(input: List<String>): Int {
+    fun calculateType2(hand: String): Int =
+        "23456789TQKA".maxOf { calculateType(hand.replace("J", "$it")) }
+
+    fun calculateStrength2(hand: String): Int =
+        hand.fold(0) { acc, ch -> acc * 14 + "J23456789TQKA".indexOf(ch) }
+
+    val comparator = compareBy(::calculateType2).thenComparing(::calculateStrength2)
+    return solve(input, comparator)
+}
 
 val types = listOf(listOf(1, 1, 1, 2), listOf(1, 2, 2), listOf(1, 1, 3), listOf(2, 3), listOf(1, 4), listOf(5))
 
-val comparator = compareBy(::calculateType).thenComparing(::calculateStrength)
+fun calculateType(hand: String): Int =
+    types.indexOf(hand.groupBy { it }.values.map { it.size }.sorted())
 
-private fun part2(input: List<String>): Int = 0
+private fun solve(input: List<String>, cmp: Comparator<String>) =
+    input.map(::parseHand)
+        .sortedWith { (hand1, _), (hand2, _) -> cmp.compare(hand1, hand2) }
+        .mapIndexed { i, (_, bid) -> (i + 1) * bid }
+        .sum()
+
+private fun parseHand(line: String) = line.split(" ").let { (hand, bid) -> hand to bid.trim().toInt() }
 
 
