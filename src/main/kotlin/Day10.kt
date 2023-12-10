d07bba077b328ba0dfc1b38c2279f5585c21c9cd
mkulak <kleshney@gmail.com>
2023-12-10 20:55:07 +0100

    make solution for day 10 completely unreadable for the sake of code golf

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -8,30 +8,26 @@ private fun part1(input: String): Int = findLoop(input).first.size / 2
 
 private fun part2(input: String): Int {
     val (loop, w) = findLoop(input)
-    val modified = input.indices.map { i -> if (i in loop) input[i] else '.' }.joinToString("")
-    return modified.indices.count { i ->
-        modified[i] == '.' && modified.substring(i + 1, ((i + 1) / w + 1) * w - 1).calcIntersections() % 2 == 1
-    }
+    val map = input.indices.map { i -> if (i in loop) input[i] else '.' }.joinToString("")
+    return map.indices.count { i -> map[i] == '.' && map.substring(i + 1, ((i + 1) / w + 1) * w - 1).inter() % 2 == 1 }
 }
 
 private fun findLoop(input: String): Pair<Set<Int>, Int> {
     val w = input.indexOf('\n') + 1
-    val start = input.indexOf('S')
-    val cur = listOf(start - 1, start + 1, start - w, start + w)
-        .find { p -> p in input.indices && start in connected(input, p, w) }!!
-    return generateSequence(linkedSetOf(start, cur)) { set ->
-        if (set.addAll(connected(input, set.last, w))) set else null
-    }.last() to w
+
+    fun connected(i: Int) = when (input[i]) {
+        '|' -> listOf(i - w, i + w)
+        '-' -> listOf(i - 1, i + 1)
+        'L' -> listOf(i - w, i + 1)
+        'J' -> listOf(i - w, i - 1)
+        '7' -> listOf(i + w, i - 1)
+        'F' -> listOf(i + w, i + 1)
+        else -> emptyList()
+    }.filter { it in input.indices && ((it - i) in setOf(w, -w) || (i / w) == (it / w)) }
+
+    val s = input.indexOf('S')
+    val cur = listOf(s - 1, s + 1, s - w, s + w).find { p -> p in input.indices && s in connected(p) }!!
+    return generateSequence(linkedSetOf(s, cur)) { if (it.addAll(connected(it.last))) it else null }.last() to w
 }
 
-fun connected(input: String, i: Int, w: Int): List<Int> = when (input[i]) {
-    '|' -> listOf(i - w, i + w)
-    '-' -> listOf(i - 1, i + 1)
-    'L' -> listOf(i - w, i + 1)
-    'J' -> listOf(i - w, i - 1)
-    '7' -> listOf(i + w, i - 1)
-    'F' -> listOf(i + w, i + 1)
-    else -> emptyList()
-}.filter { it in input.indices && ((it - i) in setOf(w, -w) || (i / w) == (it / w)) }
-
-private fun String.calcIntersections(): Int = "(\\||L(-)*7|F(-)*J)".toRegex().findAll(this).count()
+private fun String.inter(): Int = "(\\||L(-)*7|F(-)*J)".toRegex().findAll(this).count()
