fca6e989216d55f2ae4295f231ae524d89efa03e
mkulak <kleshney@gmail.com>
2023-12-10 20:48:06 +0100

    golfify solution for day 10

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -1,39 +1,37 @@
 fun main() {
-    val input = readInput("Day10")
+    val input = readInputAsText("Day10")
     println(part1(input))
     println(part2(input))
 }
 
-private fun part1(input: List<String>): Int = findLoop(input).size / 2
+private fun part1(input: String): Int = findLoop(input).first.size / 2
 
-private fun part2(input: List<String>): Int {
-    val loop = findLoop(input)
-    return input.indices.map { y ->
-        input[y].indices.map { x -> if (XY(x, y) in loop) input[y][x] else '.' }.joinToString("")
-    }.sumOf { line ->
-        line.indices.count { line[it] == '.' && line.drop(it + 1).calcIntersections() % 2 == 1 }
+private fun part2(input: String): Int {
+    val (loop, w) = findLoop(input)
+    val modified = input.indices.map { i -> if (i in loop) input[i] else '.' }.joinToString("")
+    return modified.indices.count { i ->
+        modified[i] == '.' && modified.substring(i + 1, ((i + 1) / w + 1) * w - 1).calcIntersections() % 2 == 1
     }
 }
 
-private fun findLoop(input: List<String>): Set<XY> {
-    val sy = input.indexOfFirst { 'S' in it }
-    val sx = input[sy].indexOf('S')
-    val start = XY(sx, sy)
-    val cur = listOf(XY(sx - 1, sy), XY(sx + 1, sy), XY(sx, sy - 1), XY(sx, sy + 1))
-        .find { p -> p.y in input.indices && p.x in input[p.y].indices && start in p.connected(input) }!!
+private fun findLoop(input: String): Pair<Set<Int>, Int> {
+    val w = input.indexOf('\n') + 1
+    val start = input.indexOf('S')
+    val cur = listOf(start - 1, start + 1, start - w, start + w)
+        .find { p -> p in input.indices && start in connected(input, p, w) }!!
     return generateSequence(linkedSetOf(start, cur)) { set ->
-        if (set.addAll(set.last.connected(input))) set else null
-    }.last()
+        if (set.addAll(connected(input, set.last, w))) set else null
+    }.last() to w
 }
 
-fun XY.connected(input: List<String>): List<XY> = when (input[y][x]) {
-    '|' -> listOf(XY(x, y - 1), XY(x, y + 1))
-    '-' -> listOf(XY(x - 1, y), XY(x + 1, y))
-    'L' -> listOf(XY(x, y - 1), XY(x + 1, y))
-    'J' -> listOf(XY(x, y - 1), XY(x - 1, y))
-    '7' -> listOf(XY(x, y + 1), XY(x - 1, y))
-    'F' -> listOf(XY(x, y + 1), XY(x + 1, y))
+fun connected(input: String, i: Int, w: Int): List<Int> = when (input[i]) {
+    '|' -> listOf(i - w, i + w)
+    '-' -> listOf(i - 1, i + 1)
+    'L' -> listOf(i - w, i + 1)
+    'J' -> listOf(i - w, i - 1)
+    '7' -> listOf(i + w, i - 1)
+    'F' -> listOf(i + w, i + 1)
     else -> emptyList()
-}
+}.filter { it in input.indices && ((it - i) in setOf(w, -w) || (i / w) == (it / w)) }
 
 private fun String.calcIntersections(): Int = "(\\||L(-)*7|F(-)*J)".toRegex().findAll(this).count()
