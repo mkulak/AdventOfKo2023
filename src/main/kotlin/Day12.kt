1ddc1e0ae822caa0f17ebba0c57da7c18acea815
mkulak <kleshney@gmail.com>
2023-12-15 18:44:26 +0100

    beautify solution for day 12

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -3,51 +3,38 @@ fun main() {
     println(part1(input)) // 7732
     println(part2(input)) // 4500070301581
 }
+
 private fun part1(input: List<String>) = solve(input, 1)
 
 private fun part2(input: List<String>) = solve(input, 5)
 
 private fun solve(input: List<String>, mul: Int) = input.sumOf { line ->
-    val (str, nums) = line.split(" ")
-    val fullStr = List(mul) { str }.joinToString("?")
+    val (pattern, nums) = line.split(" ")
+    val str = List(mul) { pattern }.joinToString("?")
     val clues = nums.split(",").map { it.toInt() }.let { list -> (1..mul).flatMap { list } }
-    solveRec(fullStr, clues, GroupState.NotStarted)
+    cache.clear()
+    solveRec(str, 0, clues, State.NotStarted)
 }
 
-enum class GroupState { NotStarted, Pending, Ended }
+private enum class State { NotStarted, Pending, Ended }
 
-val cache = HashMap<Triple<String, List<Int>, GroupState>, Long>()
+private val cache = HashMap<Triple<Int, List<Int>, State>, Long>()
 
-fun solveRec(line: String, left: List<Int>, state: GroupState): Long {
-    val key = Triple(line, left, state)
-    val answer = cache[key]
-    if (answer != null) return answer
-    if (left.size * 2 - 1 > line.length) return 0
-    if (left.isEmpty()) return if ("#" in line) 0 else 1
-    if (line.isEmpty()) return 0
-    val next = line.substring(1)
-    val res = when (line.first()) {
-        '.' -> {
-            if (state == GroupState.Pending) 0
-            else solveRec(next, left, GroupState.NotStarted)
-        }
-        '#' -> {
-            if (state == GroupState.Ended) 0
-            else if (left[0] > 1) solveRec(next, left.decHead(), GroupState.Pending)
-            else /*left[0] == 1*/ solveRec(next, left.drop(1), GroupState.Ended)
-        }
-        else -> {
-            if (state == GroupState.Pending) {
-                if (left[0] > 1) solveRec(next, left.decHead(), GroupState.Pending)
-                else /*left[0] == 1*/ solveRec(next, left.drop(1), GroupState.Ended)
-            } else if (state == GroupState.Ended) solveRec(next, left, GroupState.NotStarted)
-            else /*state == GroupState.NotStarted*/ solveRec(next, left, GroupState.NotStarted) +
-                if (left[0] > 1) solveRec(next, left.decHead(), GroupState.Pending)
-                else solveRec(next, left.drop(1), GroupState.Ended)
-        }
+private fun solveRec(s: String, i: Int, left: List<Int>, state: State): Long = cache.getOrPut(Triple(i, left, state)) {
+    when {
+        left.isEmpty() -> if (s.indexOf('#', i) == -1) 1 else 0
+        i == s.length -> 0
+        s[i] == '.' && state == State.Pending -> 0
+        s[i] == '.' -> solveRec(s, i + 1, left, State.NotStarted)
+        s[i] == '#' && state == State.Ended -> 0
+        s[i] == '#' && left[0] > 1 -> solveRec(s, i + 1, left.decHead(), State.Pending)
+        s[i] == '#' -> solveRec(s, i + 1, left.drop(1), State.Ended)
+        state == State.Pending && left[0] > 1 -> solveRec(s, i + 1, left.decHead(), State.Pending)
+        state == State.Pending -> solveRec(s, i + 1, left.drop(1), State.Ended)
+        state == State.Ended -> solveRec(s, i + 1, left, State.NotStarted)
+        left[0] > 1 -> solveRec(s, i + 1, left, State.NotStarted) + solveRec(s, i + 1, left.decHead(), State.Pending)
+        else -> solveRec(s, i + 1, left, State.NotStarted) + solveRec(s, i + 1, left.drop(1), State.Ended)
     }
-    cache[key] = res
-    return res
 }
 
 private fun List<Int>.decHead(): List<Int> = List(size) { this[it] - if (it == 0) 1 else 0 }
