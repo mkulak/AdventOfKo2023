944fefdd588e575502787fea33b636e1022b14c4
mkulak <kleshney@gmail.com>
2023-12-19 18:12:37 +0100

    optimize solution for day 19

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -4,22 +4,20 @@ import kotlin.math.min
 fun main() {
     val input = readInputAsText("Day19")
     println(part1(input)) // 425811
-    println(part2(input))
+    println(part2(input)) // 131796824371749
 }
 
 private fun part1(input: String): Int {
     val (pipelinesStr, partsStr) = input.split("\n\n")
     val pipelines = pipelinesStr.lines().map(::parsePipeline).toMap()
     val parts = partsStr.lines().map(::parsePart)
-    return parts.filter { pipelines.accept("in", it) }.sumOf { p -> p.x + p.m + p.a + p.s }
+    return parts.filter { pipelines.accepts("in", it) }.sumOf { it.values.sum() }
 }
 
-private tailrec fun Map<String, List<Rule>>.accept(id: String, part: Part): Boolean =
-    when (val nextId = getValue(id).first { it.predicate(part) }.next) {
-        "A" -> true
-        "R" -> false
-        else -> this@accept.accept(nextId, part)
-    }
+private fun part2(input: String): Long {
+    val pipelines = input.substringBefore("\n\n").lines().map(::parsePipeline).toMap()
+    return pipelines.countAccepted("xmas".associateWith { 1..4000 }, "in")
+}
 
 fun parsePipeline(s: String) = s.substringBefore("{") to s.substringAfter("{").dropLast(1).split(",").map { ruleStr ->
     if (":" in ruleStr) {
@@ -29,18 +27,27 @@ fun parsePipeline(s: String) = s.substringBefore("{") to s.substringAfter("{").d
 }
 
 fun parsePart(str: String): Part {
-    val (x, m, a, s) = """\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}""".toRegex().matchEntire(str)!!.destructured
-    return Part(x.toInt(), m.toInt(), a.toInt(), s.toInt())
+    val values = """\{x=(\d+),m=(\d+),a=(\d+),s=(\d+)}""".toRegex().matchEntire(str)!!.destructured.toList()
+    return "xmas".toList().zip(values) { ch, value -> ch to value.toInt() }.toMap()
 }
 
-data class Part(val x: Int, val m: Int, val a: Int, val s: Int) {
-    operator fun get(field: Char): Int = when (field) {
-        'x' -> x
-        'm' -> m
-        'a' -> a
-        else -> s
+private tailrec fun Pipeline.accepts(id: String, part: Part): Boolean =
+    when (id) {
+        "A" -> true
+        "R" -> false
+        else -> accepts(getValue(id).first { it.predicate(part) }.next, part)
     }
-}
+
+private fun Pipeline.countAccepted(ranges: PartRanges, id: String): Long =
+    when (id) {
+        "A" -> ranges.values.fold(1) { acc, r -> acc * r.size() }
+        "R" -> 0
+        else -> getValue(id).fold(0L to ranges) { (count, lastRange), rule ->
+            val (matched, nonMatched) = rule.predicate.split(lastRange)
+            val inc = if (matched.size() > 0) countAccepted(matched, rule.next) else 0
+            (count + inc) to nonMatched
+        }.first
+    }
 
 data class Rule(val next: String, val predicate: PartPredicate)
 
@@ -50,13 +57,13 @@ data class Greater(val field: Char, val value: Int) : PartPredicate
 data object True : PartPredicate
 
 operator fun PartPredicate.invoke(part: Part): Boolean = when (this) {
-    is Less -> part.get(field) < value
-    is Greater -> part.get(field) > value
+    is Less -> part.getValue(field) < value
+    is Greater -> part.getValue(field) > value
     is True -> true
 }
 
 fun PartPredicate.split(ranges: PartRanges): Pair<PartRanges, PartRanges> = when (this) {
-    is True -> ranges to empty
+    is True -> ranges to "xmas".associateWith { 0..0 }
     is Less -> {
         val range = ranges.getValue(field)
         val less = range.first..min(range.last, value - 1)
@@ -75,26 +82,11 @@ fun PartPredicate.split(ranges: PartRanges): Pair<PartRanges, PartRanges> = when
     }
 }
 
+typealias Part = Map<Char, Int>
 typealias PartRanges = Map<Char, IntRange>
-
-val empty: PartRanges = "xmas".associateWith { 0..0 }
+typealias Pipeline = Map<String, List<Rule>>
 
 private fun IntRange.size(): Int = last - first + 1
 private fun PartRanges.size(): Int = values.sumOf { it.size() }
 
-private fun part2(input: String): Long {
-    val pipelines = input.substringBefore("\n\n").lines().map(::parsePipeline).toMap()
-    //    println(pipelines)
-    fun countAccepted(ranges: PartRanges, id: String): Long {
-        if (id == "A") return ranges.values.fold(1) { acc, r -> acc * r.size() }
-        if (id == "R") return 0
-        val pipeline = pipelines.getValue(id)
-        return pipeline.fold(0L to ranges) { (count, lastRange), rule ->
-            val (matched, nonMatched) = rule.predicate.split(lastRange)
-            val inc = if (matched.size() > 0) countAccepted(matched, rule.next) else 0
-            (count + inc) to nonMatched
-        }.first
-    }
-    return countAccepted("xmas".associateWith { 1..4000 }, "in")
-}
 
