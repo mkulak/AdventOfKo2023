cf36ac020a3ddc8e4934ff5add55e8b7e26c7b3a
mkulak <kleshney@gmail.com>
2023-12-19 17:21:39 +0100

    add solution for day 19 part 1

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -1,38 +1,31 @@
 fun main() {
     val input = readInputAsText("Day19")
-    println(part1(input))
+    println(part1(input)) // 425811
     println(part2(input))
 }
 
 private fun part1(input: String): Int {
     val (pipelinesStr, partsStr) = input.split("\n\n")
-    val pipelines = pipelinesStr.lines().map(::parsePipeline).associateBy { it.id }
+    val pipelines = pipelinesStr.lines().map(::parsePipeline).toMap()
     val parts = partsStr.lines().map(::parsePart)
-    return parts.filter { accept(pipelines, it) }.sumOf { p -> p.x + p.m + p.a + p.s }
+    return parts.filter { pipelines.accept("in", it) }.sumOf { p -> p.x + p.m + p.a + p.s }
 }
 
-fun accept(pipelines: Map<String, Pipeline>, part: Part): Boolean {
-    var pipeline: Pipeline = pipelines["in"]!!
-    while (true) {
-        val nextId = pipeline.rules.firstOrNull { it.predicate(part) }?.next
-        if (nextId == "A") return true
-        if (nextId == "R") return false
-        pipeline = pipelines[nextId]!!
+private tailrec fun Map<String, List<Rule>>.accept(id: String, part: Part): Boolean =
+    when (val nextId = getValue(id).first { it.predicate(part) }.next) {
+        "A" -> true
+        "R" -> false
+        else -> this@accept.accept(nextId, part)
     }
-}
 
-fun parsePipeline(s: String): Pipeline {
-    val id = s.substringBefore("{")
-    val rules = s.substringAfter("{").dropLast(1).split(",").map { ruleStr ->
-        if (":" in ruleStr) {
-            val next = ruleStr.substringAfter(":")
-            val const = ruleStr.substringBefore(":").drop(2).toInt()
-            val f = ruleStr[0]
-            val prop = if (f == 'x') Part::x else if (f == 'm') Part::m else if (f == 'a') Part::a else Part::s
-            Rule(next) { part -> if (ruleStr[1] == '>') prop.get(part) > const else prop.get(part) < const }
-        } else Rule(ruleStr) { true }
-    }
-    return Pipeline(id, rules)
+fun parsePipeline(s: String) = s.substringBefore("{") to s.substringAfter("{").dropLast(1).split(",").map { ruleStr ->
+    if (":" in ruleStr) {
+        val next = ruleStr.substringAfter(":")
+        val const = ruleStr.substringBefore(":").drop(2).toInt()
+        val f = ruleStr[0]
+        val prop = if (f == 'x') Part::x else if (f == 'm') Part::m else if (f == 'a') Part::a else Part::s
+        Rule(next) { part -> if (ruleStr[1] == '>') prop.get(part) > const else prop.get(part) < const }
+    } else Rule(ruleStr) { true }
 }
 
 fun parsePart(str: String): Part {
@@ -42,8 +35,10 @@ fun parsePart(str: String): Part {
 
 data class Part(val x: Int, val m: Int, val a: Int, val s: Int)
 
-data class Pipeline(val id: String, val rules: List<Rule>)
-
 data class Rule(val next: String, val predicate: (Part) -> Boolean)
 
-private fun part2(input: String): Int = 0
+private fun part2(input: String): Int {
+    val pipelines = input.substringBefore("\n\n").lines().map(::parsePipeline).toMap()
+//    println(pipelines)
+    return 0
+}
