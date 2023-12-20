b2c50fa5b24c32f08a559f68fa07c228ffc8462a
mkulak <kleshney@gmail.com>
2023-12-20 11:55:44 +0100

    beautify solution for day 19

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -20,8 +20,8 @@ fun parsePipeline(s: String) = s.substringBefore("{") to s.substringAfter("{").d
     if (":" in ruleStr) {
         val isLess = ruleStr[1] == '<'
         val v = ruleStr.substringBefore(":").drop(2).toInt() + if (isLess) 0 else 1
-        Rule(ruleStr.substringAfter(":"), Compare(ruleStr[0], v, isLess))
-    } else Rule(ruleStr, True)
+        Rule(ruleStr.substringAfter(":"), ruleStr[0], v, isLess)
+    } else Rule(ruleStr, 'x', 0, false)
 }
 
 fun parsePart(str: String): Part {
@@ -33,45 +33,36 @@ private tailrec fun Pipeline.accepts(id: String, part: Part): Boolean =
     when (id) {
         "A" -> true
         "R" -> false
-        else -> accepts(getValue(id).first { it.predicate(part) }.next, part)
+        else -> accepts(getValue(id).first { it.matches(part) }.next, part)
     }
 
 private fun Pipeline.countAccepted(ranges: PartRanges, id: String): Long =
     when (id) {
-        "A" -> ranges.values.fold(1) { acc, r -> acc * r.size() }
+        "A" -> ranges.values.fold(1) { acc, range -> acc * range.size() }
         "R" -> 0
         else -> getValue(id).fold(0L to ranges) { (count, lastRange), rule ->
-            val (matched, nonMatched) = rule.predicate.split(lastRange)
+            val (matched, nonMatched) = rule.split(lastRange)
             val inc = if (matched.empty()) 0 else countAccepted(matched, rule.next)
             (count + inc) to nonMatched
         }.first
     }
 
-operator fun PartPredicate.invoke(part: Part): Boolean = when (this) {
-    is True -> true
-    is Compare -> if (isLess) part.getValue(field) < value else part.getValue(field) >= value
-}
+data class Rule(val next: String, val field: Char, val value: Int, val isLess: Boolean)
 
-fun PartPredicate.split(ranges: PartRanges): Pair<PartRanges, PartRanges> = when (this) {
-    is True -> ranges to "xmas".associateWith { 0..0 }
-    is Compare -> {
-        val range = ranges.getValue(field)
-        val less = range.first..range.last.coerceAtMost(value - 1)
-        val more = range.first.coerceAtLeast(value)..range.last
-        val rangesWithLess = ranges + (field to less)
-        val rangesWithMore = ranges + (field to more)
-        if (isLess) rangesWithLess to rangesWithMore else rangesWithMore to rangesWithLess
-    }
+fun Rule.matches(part: Part): Boolean =
+    if (isLess) part.getValue(field) < value else part.getValue(field) >= value
+
+fun Rule.split(ranges: PartRanges): Pair<PartRanges, PartRanges> {
+    val range = ranges.getValue(field)
+    val less = range.first..range.last.coerceAtMost(value - 1)
+    val more = range.first.coerceAtLeast(value)..range.last
+    val rangesWithLess = ranges + (field to less)
+    val rangesWithMore = ranges + (field to more)
+    return if (isLess) rangesWithLess to rangesWithMore else rangesWithMore to rangesWithLess
 }
 
 private fun IntRange.size(): Int = last - first + 1
 private fun PartRanges.empty(): Boolean = values.all { it.size() == 0 }
-
-data class Rule(val next: String, val predicate: PartPredicate)
-
-sealed interface PartPredicate
-data class Compare(val field: Char, val value: Int, val isLess: Boolean) : PartPredicate
-data object True : PartPredicate
 
 typealias Part = Map<Char, Int>
 typealias PartRanges = Map<Char, IntRange>
