c6ed42bf96774cbdb8d8cafc1b3d79a53f44405e
mkulak <kleshney@gmail.com>
2023-12-20 11:44:14 +0100

    beautify solution for day 18

--- src/main/kotlin/Day18.kt
+++ src/main/kotlin/Day18.kt
@@ -4,129 +4,64 @@ import kotlin.math.min
 fun main() {
     val input = readInput("Day18")
     println(part1(input)) // 67891
-//    println(part2(input)) //
+    println(part2(input)) // 94116351948493
 }
 
-private fun part1(input: List<String>): Int {
-    val steps = input.map { parseDir(it.first()) to it.drop(2).substringBefore(" ").toInt() }
-    val verticles = steps.fold(mutableListOf(XY(0, 0))) { acc, (dir, step) ->
-        acc.add(acc.last() + XY(dir.dx * step, dir.dy * step))
-        acc
+private fun part1(input: List<String>): Long {
+    val steps = input.map { line ->
+        Dir.entries.first { it.name[0] == line[0] } to line.drop(2).substringBefore(" ").toLong()
     }
-    val maxX = verticles.maxOf { it.x }
-    val maxY = verticles.maxOf { it.y }
-    val minX = verticles.minOf { it.x }
-    val minY = verticles.minOf { it.y }
-    val adjMaxX = maxX - minX
-    val adjMaxY = maxY - minY
-
-    println(verticles.size)
-    println("${XY(minX, minY)} ${XY(maxX, maxY)}")
-    val adjVerticles = verticles.map { (x, y) -> XY(x - minX, y - minY) }
-    println(adjVerticles)
-
-    val (vertical, horizontal) = adjVerticles.zipWithNext().partition { (a, b) -> a.x == b.x }
-
-    val verticlesSet = (0..adjMaxY).flatMap { y ->
-        (0..adjMaxX).filter { x ->
-            horizontal.any { (a, b) -> a.y == y && x.isBetweenInclusive(a.x, b.x) } ||
-                vertical.any { (a, b) -> x == a.x && y.isBetweenInclusive(a.y, b.y) }
-        }.map { XY(it, y) }
-    }.toSet()
-
-    val insideSet = (0..adjMaxY).flatMap { y ->
-        (0..adjMaxX).filter { x ->
-            XY(x, y) !in verticlesSet && vertical.indices.count { i ->
-                val v = vertical[i]
-                val vNext = if (i < vertical.lastIndex) vertical[i + 1] else vertical[0]
-                x < v.first.x
-                    && (y.isBetweenExclusive(v.first.y, v.second.y) || (y == v.second.y && v.vdir() == vNext.vdir()))
-            } % 2 == 1
-        }.map { XY(it, y) }
-    }.toSet()
-    debugPrint(adjMaxX, adjMaxY, verticlesSet, insideSet)
-    return verticlesSet.size + insideSet.size
+    return solve(steps)
 }
-
-fun debugPrint(maxX: Int, maxY: Int, verticlesSet: Set<XY>, insideSet: Set<XY>) {
-    (0..maxY).forEach { y ->
-        (0..maxX).forEach { x ->
-            val edge = XY(x, y) in verticlesSet
-            val inside = XY(x, y) in insideSet
-            val color = when {
-                edge && inside -> ANSI_PURPLE
-                edge -> ANSI_RED
-                inside -> ANSI_GREEN
-                else -> ANSI_BLACK
-            }
-            val ch = if (edge || inside) '#' else "."
-            print(color + ch + ANSI_RESET)
-        }
-        println()
-    }
-}
-
-private fun Pair<XY, XY>.vdir(): Dir = if (first.y < second.y) Dir.Down else Dir.Up
-private fun Pair<XYL, XYL>.vdir2(): Dir = if (first.y < second.y) Dir.Down else Dir.Up
-
-fun Int.isBetweenInclusive(a: Int, b: Int) = this in min(a, b)..max(a, b)
-fun Int.isBetweenExclusive(a: Int, b: Int) = this in min(a + 1, b + 1)..max(a - 1, b - 1)
-
-fun Long.isBetweenInclusive(a: Long, b: Long) = this in min(a, b)..max(a, b)
-fun Long.isBetweenExclusive(a: Long, b: Long) = this in min(a + 1, b + 1)..max(a - 1, b - 1)
-
-private fun parseDir(ch: Char): Dir = Dir.entries.first { it.name.first() == ch }
-private fun parseDir2(ch: Char): Dir = listOf(Dir.Right, Dir.Down, Dir.Left, Dir.Up)[ch.code - '0'.code]
 
 private fun part2(input: List<String>): Long {
     val steps = input.map {
         val hex = it.substringAfter('#').dropLast(1)
-        val steps = hex.dropLast(1).toLong(16)
-        val dir = parseDir2(hex.last())
-//        0 means R, 1 means D, 2 means L, and 3 means U.
-//        Up(0, -1), Left(-1, 0), Down(0, 1), Right(1, 0);
-        dir to steps
+        val dir = listOf(Dir.Right, Dir.Down, Dir.Left, Dir.Up)[hex.last().code - '0'.code]
+        dir to hex.dropLast(1).toLong(16)
     }
-    val verticles = steps.fold(mutableListOf(XYL(0, 0))) { acc, (dir, step) ->
-        acc.add(acc.last() + XYL(dir.dx * step, dir.dy * step))
-        acc
+    return solve(steps)
+}
+
+private fun solve(steps: List<Pair<Dir, Long>>): Long {
+    val edge = steps.fold(listOf(XYL(0, 0))) { acc, (dir, step) ->
+        acc + (acc.last() + XYL(dir.dx * step, dir.dy * step))
     }
-    val maxX = verticles.maxOf { it.x }
-    val maxY = verticles.maxOf { it.y }
-    val minX = verticles.minOf { it.x }
-    val minY = verticles.minOf { it.y }
-    val adjMaxX = maxX - minX
-    val adjMaxY = maxY - minY
+    val xs = edge.map { it.x }.toSet().sorted() + edge.maxOf { it.x + 1 }
+    val ys = edge.map { it.y }.toSet().sorted() + edge.maxOf { it.y + 1 }
+    val (vertical, horizontal) = edge.zipWithNext().partition { (a, b) -> a.x == b.x }
 
-    val adjVerticles = verticles.map { (x, y) -> XYL(x - minX, y - minY) }
-    println(adjVerticles)
-    println("max xy: [$adjMaxX, $adjMaxY]")
+    fun isEdge(x: Long, y: Long) =
+        vertical.any { it.first.x == x && y in it.ys } || horizontal.any { it.first.y == y && x in it.xs }
 
-    val (vertical, horizontal) = adjVerticles.zipWithNext().partition { (a, b) -> a.x == b.x }
+    fun isInside(x: Long, y: Long) =
+        vertical.indices.count { i ->
+            val v1 = vertical[i]
+            val v2 = vertical[(i + 1) % vertical.size]
+            x < v1.first.x && (y in v1.ys.exlusive || y == v1.second.y && v1.vdir == v2.vdir)
+        } % 2 == 1
 
-    val verticlesSet = (0..adjMaxY).flatMap { y ->
-        (0..adjMaxX).filter { x ->
-            horizontal.any { (a, b) -> a.y == y && x.isBetweenInclusive(a.x, b.x) } ||
-                vertical.any { (a, b) -> x == a.x && y.isBetweenInclusive(a.y, b.y) }
-        }.map { XYL(it, y) }
-    }.toSet()
-
-    val insideCount = (0..adjMaxY).sumOf { y ->
-        (0..adjMaxX).sumOf { x ->
-            val fit = XYL(x, y) !in verticlesSet && vertical.indices.count { i ->
-                val v = vertical[i]
-                val vNext = if (i < vertical.lastIndex) vertical[i + 1] else vertical[0]
-                x < v.first.x
-                    && (y.isBetweenExclusive(v.first.y, v.second.y) || (y == v.second.y && v.vdir2() == vNext.vdir2()))
-            } % 2 == 1
-            if (fit) 1L else 0L
+    return ys.zipWithNext().sumOf { (y1, y2) ->
+        xs.zipWithNext().sumOf { (x1, x2) ->
+            when {
+                isEdge(x2 - 1, y2 - 1) || isInside(x2 - 1, y2 - 1) -> (x2 - x1) * (y2 - y1)
+                isEdge(x1, y2 - 1) && isEdge(x2 - 1, y1) -> x2 - x1 + y2 - y1 - 1
+                isEdge(x1, y2 - 1) -> y2 - y1
+                isEdge(x2 - 1, y1) -> x2 - x1
+                isEdge(x1, y1) -> 1
+                else -> 0
+            }
         }
     }
-    return verticlesSet.size + insideCount
 }
 
-data class XYL(val x: Long, val y: Long) {
-    operator fun plus(xy: XYL) = XYL(x + xy.x, y + xy.y)
-    override fun toString(): String = "[$x, $y]"
-}
+private typealias Segment = Pair<XYL, XYL>
+private val Segment.vdir: Dir get() = if (first.y < second.y) Dir.Down else Dir.Up
+private val Segment.xs: LongRange get() = min(first.x, second.x)..max(first.x, second.x)
+private val Segment.ys: LongRange get() = min(first.y, second.y)..max(first.y, second.y)
+
+private val LongRange.exlusive: LongRange get() = (first + 1)..(last - 1)
+
+private data class XYL(val x: Long, val y: Long)
+private operator fun XYL.plus(xy: XYL): XYL = XYL(x + xy.x, y + xy.y)
 
